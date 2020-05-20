ef0a702989b3bd028552d890a7b172c7037e6fc1
Amrit Singh <asingh.analytics@gmail.com>
2020-05-20 17:35:05 -0400

    run app using kubectl

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="singha53/api"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment api --image="$dockerpath:version1.0"
+kubectl expose deployment/api --port=80 --target-port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --sort-by='.status.containerStatuses[0].restartCount'
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/api 8000:80
